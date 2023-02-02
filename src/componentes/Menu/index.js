ce514e84fafc9503522ba6dba7325971d69c1fdd
Evandro <096066330647@tre-pr.jus.br>
2023-02-02 14:18:52 -0300

    Criando o componente de Tags e o da Galeria

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,34 +1,14 @@
-import home from '../../assets/icones/home.png';
-import maisCurtidas from '../../assets/icones/mais-curtidas.png';
-import maisVistas from '../../assets/icones/mais-vistas.png';
-import novas from '../../assets/icones/novas.png';
-import surpreendaMe from '../../assets/icones/surpreenda-me.png';
 import styles from './Menu.module.scss';
+import icones from './icones.json';
+import Icone from './Icone';
 
 export default function Menu() {
     return (
         <nav className={styles.menu}>
-            <ul className={styles.menu__list}>
-                <li className={styles.menu__item}>
-                    <img src={home} alt="" />
-                    <a href='/'>Início</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisCurtidas} alt="" />
-                    <a href='/'>Mais curtidas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisVistas} alt="" />
-                    <a href='/'>Mais vistas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={novas} alt="" />
-                    <a href='/'>Novas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={surpreendaMe} alt="" />
-                    <a href='/'>Surpreenda-me</a>
-                </li>
+            <ul className={styles.menu__lista}>
+                {icones.map(icone => (
+                    <Icone key={icone.id} icone={icone} styles={styles} />
+                ))}
             </ul>
         </nav>
     )
